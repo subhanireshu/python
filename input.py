fe8933af9873aae09572a1103b3d22bfc3630384
subhanireshu <subhanireshu.com>
2026-03-19 22:33:24 +0530

    Modifyed the input program

--- input.py
+++ input.py
@@ -1,7 +1,4 @@
 name=input("Enter the name:")
 age=int(input("Enter the age:"))
-price=float(input("Enter the price:"))
 
-print(name)
-print(age)
-print(price)
+print("My name is ", name, "and I am  ",age, "years old")
