cec5cd07dc0b5ff617220b2574ba1ba27418f443
subhanireshu <subhanireshu.com>
2026-03-19 07:08:12 +0530

    Primenumber program

---
+++ max_of_three.py
@@ -0,0 +1,10 @@
+a=int(input("Enter the 1st Number:"))
+b=int(input("Enter the 2nd Number:"))
+c=int(input("Enter the 3rd Number:"))
+if (a>b) and (a>c):
+    print(a, "Is the Maximum Number")
+elif (b>c):
+    print(b, "Is the Maximum Number")
+else:
+    print(c, "Is the Maximum Number")
+
