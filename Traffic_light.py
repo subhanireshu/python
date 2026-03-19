f9ea26c35e9e4425de63ea1c5c2e329d1065975b
subhanireshu <subhanireshu.com>
2026-03-19 23:20:18 +0530

    Trafic light program IfElifElse

---
+++ Traffic_light.py
@@ -0,0 +1,9 @@
+light=input("Enter the Light Clour:")
+if(light=="Red"):
+    print("STOP the car signal light is RED")
+elif(light=="Green"):
+    print("GO , Signal light showing green ")
+elif(light=="Yellow"):
+    print("Look the Signals showing yellow light")
+else:
+    print("Signal Lights are broken ")
