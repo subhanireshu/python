a0f0bf891b714a4d53eba0acbd283ee3fdcf4223
subhanireshu <subhanireshu.com>
2026-03-20 05:57:15 +0530

    Ternary operatior program

---
+++ Ternary_Operator.py
@@ -0,0 +1,3 @@
+food=input("Food:")
+eat="YES"if food=="cake" else "NO"
+print(eat)
---
+++ Ternary_Operator2.py
@@ -0,0 +1,2 @@
+food=input("Food:")
+print(food, " is a Sweet item") if food=="cake" or food=="Jelabi" else print(food, "Not a sweet item")
